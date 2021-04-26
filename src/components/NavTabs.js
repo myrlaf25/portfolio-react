12a37058936a7bc9e3b863bf5140b1aa051bc8b6
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-25 18:27:00 -0700

    working on pages

---
+++ src/components/NavTabs.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+function NavTabs(props) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => props.handlePageChange("Home")}
+          className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#aboutme"
+          onClick={() => props.handlePageChange("AboutMe")}
+          className={props.currentPage === "AboutMe" ? "nav-link active" : "nav-link"}
+        >
+          About Me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#mywork"
+          onClick={() => props.handlePageChange("My Work")}
+          className={props.currentPage === "MyWork" ? "nav-link active" : "nav-link"}
+        >
+          My Work
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#skills"
+          onClick={() => props.handlePageChange("Skills")}
+          className={props.currentPage === "Skills" ? "nav-link active" : "nav-link"}
+        >
+          Skills
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contactme"
+          onClick={() => props.handlePageChange("ContactMe")}
+          className={props.currentPage === "ContactMe" ? "nav-link active" : "nav-link"}
+        >
+          Contact Me
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
+import MyWork from "./pages/MyWork";
+import Skills from "./pages/Skills";
+import ContactMe from "./pages/ContactMe";
+
+class Portfolio extends Component {
+  state = {
+    currentPage: "Home"
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  renderPage = () => {
+    if (this.state.currentPage === "Home") {
+      return <Home />;
+    } else if (this.state.currentPage === "AboutMe") {
+      return <AboutMe/>;
+    } else if (this.state.currentPage === "MyWork") {
+      return <MyWork />;
+    } else if (this.state.currentPage === "Skills") {
+        return <Skills/>;
+    } else {
+      return <ContactMe/>;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <NavTabs
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange}
+        />
+        {this.renderPage()}
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
