d920ac60961beec1d9f96c9450cd76200700857e
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-27 19:08:24 -0700

    resume

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -16,18 +16,18 @@ const AboutMe = () => (
   <div className="flexRow justify-sb align-center">
     <div>
     <h1 className="textColor mtb-10">About Me</h1>
-    <p className="bold-700 mtb-10 font-25" >
+    <p className="bold-700 mtb-10 font-25 ptb" >
     Hello, my name is Myrla Flores. I am new to the web development world.
     I am currently enrolled in the Full Stack Coding Bootcamp through the
     University of Arizona.
   </p>
-  <p className="bold-700 mtb-10 font-25" >
+  <p className="bold-700 mtb-10 font-25 ptb" >
     I currently reside in Tucson, AZ. I have a Bachelor’s of Science in
     Psychology from The University of Arizona. Most of my work experience
     in is Case Management.
   </p>
   <div>
-  <p className="bold-700 mtb-10 font-25" >
+  <p className="bold-700 mtb-10 font-25 ptb" >
     This has been a very challenging experience yet it has been very
     rewarding. I have always enjoyed creating things and computers. This
     is the reason why I decided that Web Development was for me.
--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,18 +1,23 @@
 import React from "react";
+import Resume from "../assets/resumeLogo.png"
 
 
 const Skills = () => (
  <>
     <div className='containerSkills'>
     <h3 className="bold-700 mtb-10 textCenter"style={{marginTop: '50px', fontSize: '100px', color: 'white'}}>Web Development Experience</h3>
+    <button style={{
+        borderRadius: "45px",
+        margin: "5px",
+        boxShadow: "8px 5px 2px 2px rgba(15, 15, 15, 0.73)",
+        overflow: "hidden", 
+    }}><a href="https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:76cc7368-4254-4868-b88a-d287e07a7d7c"><img src={Resume} alt="resume"/></a></button>
     </div>
-    
-   
     <div className='container skills-list'>
     <div>
     <h3 className="textCenter textColor">I have experience in the following:</h3>
     <ul className='textCenter' style={{listStyle:'none'}}>
-        <li> HTML</li>
+        <li>HTML</li>
         <li>CSS</li>
         <li>Flexbox</li>
         <li>Javascript</li>
@@ -22,7 +27,7 @@ const Skills = () => (
         <li>Node.JS</li>
         <li>Sequelize</li>
         <li>Express</li>
-        <li>and more to come...</li>
+        <li>plus others...</li>
     </ul>
 </div>
 
