d2744ef6c8707fbee779468511981fbe78f37729
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-28 17:45:49 -0700

    update

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,47 +1,52 @@
 import React from "react";
-import Me from "../assets/meAndSkids.jpg"
-const styles ={
-    imageStyle: {
+import Me from "../assets/meAndSkids.jpg";
+const styles = {
+  imageStyle: {
     height: "350px",
     borderRadius: "90px",
     margin: "40px",
     boxShadow: "8px 6px 2px 2px rgba(15, 15, 15, 0.73)",
-    }
-}
+  },
+};
 
 function AboutMe() {
   return (
-      <>
-    <div className="headerAbout">
-    <h1 className="textColor textCenter bold-700"style={{ fontSize: "80px"}}>About Me</h1>
-    <img className="aboutMePic" style={styles.imageStyle} src={Me} alt="me"/>
-    </div>  
-    <div className="containerMe" >
-    <div className="align-center ">
-    <div>
-      <p className="bold-700 mtb-130 font-25 ptb">
-      Hello, my name is Myrla Flores. I am new to the web development world.
-      I am currently enrolled in the Full Stack Coding Bootcamp through the
-      University of Arizona.
-    </p>
-    </div>
-    <div>
-    <p className="bold-700 mtb-130 font-25 ptb" >
-      I currently reside in Tucson, AZ. I have a Bachelor’s of Science in
-      Psychology from The University of Arizona. Most of my work experience
-      in is Case Management.
-    </p>
-    </div>
-    <div>
-    <p className="bold-700 mtb-130 font-25 ptb" >
-      This has been a very challenging experience yet it has been very
-      rewarding. I have always enjoyed creating things and computers. This
-      is the reason why I decided that Web Development was for me.
-    </p>
-    </div>
-    </div>
-    </div>
-   
+    <>
+      <div className="headerAbout">
+        <h1
+          className="textColor textCenter bold-700"
+          style={{ fontSize: "80px" }}
+        >
+          About Me
+        </h1>
+        <img
+          className="aboutMePic"
+          style={styles.imageStyle}
+          src={Me}
+          alt="me"
+        />
+      </div>
+      <div className="containerMe">
+        <div>
+          <h4 className="bold-500 mtb-30">
+            Hello, my name is Myrla Flores. I am new to the web development
+            world. I am currently enrolled in the Full Stack Coding Bootcamp
+            through the University of Arizona.
+          </h4>
+
+          <h4 className="bold-500 mtb-30">
+            I currently reside in Tucson, AZ. I have a Bachelor’s of Science in
+            Psychology from The University of Arizona. Most of my work
+            experience in is Case Management.
+          </h4>
+
+          <h4 className="bold-500 mtb-30">
+            This has been a very challenging experience yet it has been very
+            rewarding. I have always enjoyed creating things and computers. This
+            is the reason why I decided that Web Development was for me.
+          </h4>
+        </div>
+      </div>
     </>
   );
 }
