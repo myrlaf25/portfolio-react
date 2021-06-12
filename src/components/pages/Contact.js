031ead6954847f22a12f14987539b884a0a712c0
myrla <myrlaf25@gmail.com>
2021-06-12 11:13:16 -0700

    footer

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,7 +3,7 @@ import Me from "../assets/me.png";
 
 const styles = {
   imageStyle: {
-    height: "375px",
+    height: "250px",
     borderRadius: "90px",
     margin: "30px",
     boxShadow: "8px 6px 2px 2px rgba(15, 15, 15, 0.73)",
@@ -19,34 +19,34 @@ function Contact() {
           style={{ marginTop: "30px", fontSize: "70px", color: "#ebfaf6" }}
         >
           Contact Me
-        </h3>
-
         <img
           className="ContactPic"
           style={styles.imageStyle}
           src={Me}
           alt="me"
         />
-      </div>
+        </h3>
+</div>
 
-      <div className="container" style={{ marginTop: "10px" }}>
-        <div>
-          <ul className="contactList textCenter" style={{ listStyle: "none" }}>
-            <li className="bold-700 font-30">Myrla Flores</li>
-            <li className="font-25">Phone: (520)245-9119</li>
-            <li className="font-25">
+
+      <div className="container textCenter" style={{ marginTop: "30px" }}>
+        
+          
+            <h4 className="bold-700 font-30">Myrla Flores</h4>
+            <h4 className="font-25">Phone: (520)245-9119</h4>
+            <h4 className="font-25">
               Send me an email:{" "}
               <a href="mailto:myrlaf25@gmail.com"> myrlaf25@gmail.com.</a>
-            </li>
+            </h4>
 
             
           <h3 className="bold-700 textCenter font-30">
             Thank you for stopping by!
           </h3>
-          </ul>
-        </div>
+          </div>
+       
       </div>
-    </div>
+   
   );
 }
 
